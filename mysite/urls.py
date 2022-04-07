f6ac2fa82a00000c8507b18acdb46b85e9f602f0
RichardKim <hiddeneventor@gmail.com>
2022-04-07 22:24:57 +0900

    1. CSRF 설정 추가 2. 먜장 GET, POST REST API 완료

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('order/', include('order.urls')),
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 
 ]
--- order/views.py
+++ order/views.py
@@ -1,4 +1,20 @@
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 from django.shortcuts import render
+from order.models import Shop, Menu, Order, Orderfood
+from order.serializers import ShopSerializer
 
+@csrf_exempt
 def shop(request):
-
+    if request.method == 'GET':
+        shop = Shop.objects.all()
+        serializer = ShopSerializer(shop, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ShopSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
