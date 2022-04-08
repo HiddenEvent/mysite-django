de133b04e022ddd53707d15839cc84c976a2cfb8
RichardKim <hiddeneventor@gmail.com>
2022-04-08 15:16:52 +0900

    사장님 배달 소요시간 insert 처리

--- boss/views.py
+++ boss/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 from django.shortcuts import render
+from order.models import Shop, Menu, Order, Orderfood
+from order.serializers import ShopSerializer, MenuSerializer
 
-# Create your views here.
+@csrf_exempt
+def time_input(request):
+    if request.method == 'GET':
+        order_list = Order.objects.all()
+        return render(request, 'boss/order_list.html', {'order_list': order_list})
+    elif request.method == 'POST':
+        order_id = request.POST['order_id']
+        estimated_time = request.POST['estimated_time']
+        order_item = Order.objects.get(pk=order_id)
+        order_item.estimated_time = estimated_time
+        order_item.save()
+        return render(request, 'boss/success.html')
