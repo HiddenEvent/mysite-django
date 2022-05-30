fcd11cdc9c52734bb640208a54ad1ba5cc502704
RichardKim <hiddeneventor@gmail.com>
2022-05-31 01:23:31 +0900

    freeze 텍스트 추가

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,19 +78,25 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': '데이터베이스 이름',
-        'USER': '사용자이름',
-        'PASSWORD': '패스워드',
-        'HOST': 'RDS엔드포인트 URL 복사',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-
-        }
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': '데이터베이스 이름',
+#         'USER': '사용자이름',
+#         'PASSWORD': '패스워드',
+#         'HOST': 'RDS엔드포인트 URL 복사',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+#
+#         }
+#
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
